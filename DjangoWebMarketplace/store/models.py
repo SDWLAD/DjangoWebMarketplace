af807228af5c52a9ea0a16140ffd707eff48fb22
SDWLAD <sidorchukvladislav0@gmail.com>
2025-01-18 21:48:28 +0200

    Add category field to MarketplaceItem model and create migration

--- DjangoWebMarketplace/store/models.py
+++ DjangoWebMarketplace/store/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class MarketplaceItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    category = models.CharField(max_length=100, default="other")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
