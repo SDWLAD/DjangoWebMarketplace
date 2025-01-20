a0c6b33275293d705af8959fc4fb2fb0f0bd1137
SDWLAD <sidorchukvladislav0@gmail.com>
2025-01-20 23:25:46 +0200

    Add categories to context in IndexView for improved item filtering

--- DjangoWebMarketplace/store/views.py
+++ DjangoWebMarketplace/store/views.py
@@ -7,3 +7,8 @@ class IndexView(ListView):
     model = MarketplaceItem
     template_name = "store/index.html"
     context_object_name = 'items'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = MarketplaceItem.objects.values('category').distinct()
+        return context
